7d3a579e0cf49a56d2ddbfacd782bd0d489b5594
Nicolas Bratoveanu <nicolasbratoveanu@gmail.com>
2022-06-12 21:07:45 +0300

    Initial commit

---
+++ patch/include.h
@@ -0,0 +1,21 @@
+/* Bar functionality */
+#include "bar_indicators.h"
+#include "bar_tagicons.h"
+
+#include "combo.h"
+#include "bar_ltsymbol.h"
+#include "bar_status.h"
+#include "bar_statuscolors.h"
+#include "bar_tags.h"
+#include "bar_wintitle.h"
+
+/* Other patches */
+#include "cyclelayouts.h"
+#include "fullscreen.h"
+#include "push.h"
+#include "selfrestart.h"
+#include "vanitygaps.h"
+/* Layouts */
+#include "layout_monocle.h"
+#include "layout_tile.h"
+
